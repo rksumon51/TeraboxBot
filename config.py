f550eee0a00ed3c6d0d1d96dceb73ac186593789
rksumon51 <rk.sumon51@gmail.com>
2026-09-16 06:13:45 +0600

    Update BOT_TOKEN and MONGO_URI in config.py
    Updated BOT_TOKEN and MONGO_URI with actual values.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # os.getenv("ভেরিয়েবলের_নাম", "যদি_না_পায়_তবে_এটি_ব্যবহার_করবে")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "এখানে_আপনার_আসল_টোকেন_দিন")
-MONGO_URI = os.getenv("MONGO_URI", "এখানে_আপনার_মঙ্গোডিবি_লিংক_দিন")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8616358877:AAHJdquziRgvrOVgDNQgLa-TZ1xNl8evQvk")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://admin:5129534Yk@cluster0.0ggumii.mongodb.net/?appName=Cluster0")
 PRIMARY_OWNER_ID = int(os.getenv("PRIMARY_OWNER_ID", 8110034101))
 WEBAPP_URL = os.getenv("WEBAPP_URL", "https://terabox-bot-steel.vercel.app")
